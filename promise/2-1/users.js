3ca02540de2464d3c79d37eac3b5d9a1e98006db
mohammadgk1993 <mohammadgk1993@gmail.com>
2023-02-10 20:45:25 +0330

    2nd commit

--- promise/2-1/users.js
+++ promise/2-1/users.js
@@ -2,7 +2,7 @@ let users = []
 
 for (let i of [1,2]) {
     fetch(`https://reqres.in/api/users?page=${i}`)
-    .then(res => console.log(res))
+    .then(res => res.json())
     .then(responseData => {
         for (let i of responseData.data) {
             users.push(i)
@@ -11,3 +11,18 @@ for (let i of [1,2]) {
 }
 
 console.log(users)
+
+// (async () => {
+//     const result = await fetch(`https://reqres.in/api/users?page=1`)
+//     console.log(result)
+// })()
+
+// (async function() {
+//     try {
+//     const response = await fetch("https://reqres.in/api/users?page=1");
+//     const data = await response.json();
+//     console.log(data);
+//     } catch (error) {
+//     console.error(error);
+//     }
+//     })();
